140dee2d932866546a8c72898c980dbbcc0bea4e
priya prajapati <priyap572485@gmail.com>
2026-04-26 13:44:18 +0530

    practical7

---
+++ practical7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Employee {
+private String employeeName;
+private double employeeSalary;
+
+public void readEmployeeData() {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter Employee Name: ");
+employeeName = sc.nextLine();
+System.out.print("Enter Employee Salary: ");
+employeeSalary = sc.nextDouble();
+}
+
+public void displayEmployeeData() {
+System.out.println("\nEmployee Details");
+System.out.println("Name: " + employeeName);
+System.out.println("Salary: " + employeeSalary);
+}
+
+public static void main(String[] args) {
+Employee emp = new Employee(); 
+emp.readEmployeeData();
+emp.displayEmployeeData();
+}
+}
