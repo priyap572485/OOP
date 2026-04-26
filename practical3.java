ddc94de111f116563e3260bc3f84d5ecae8f38c0
priya prajapati <priyap572485@gmail.com>
2026-04-26 12:32:24 +0530

    practical3

---
+++ practical3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a single letter: ");
+        char ch = sc.next().charAt(0);
+        ch = Character.toLowerCase(ch);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("The entered character is a Vowel.");
+        } else {
+            System.out.println("The entered character is a Consonant.");
+        }
+
+        sc.close();
+    }
+}
